17befbae16b0314f2895e7f80c822ea5ea7b48b6
jforaker04 <github@jcrec.net>
2016-11-07 11:49:32 -0800

    Adding update to files_ex1.py

--- files_ex1.py
+++ files_ex1.py
@@ -6,4 +6,15 @@ f = open('file_list.txt')
 ## Read the lines of the file
 for line in f:
   print line.strip()
+f.close()
+
+## Create new file
+f = open('file_list_new.txt', 'w')
+f.close()
+
+## Open new file and append
+f = open('file_list_new.txt', 'a')
+f.write('This is a line appended to the end.\n')
+
+
 
