f144416d9a9bc624fec9d833ced92158cabccb9d
jforaker04 <github@jcrec.net>
2016-11-07 20:40:11 -0500

    Adding remainder day1 files

---
+++ dict_ex1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+net_device = { 
+  'ip_addr' :  '10.0.201.72',
+  'username' : 'admin',
+  'password' : ' inital_pass',
+  'vendor' : 'cisco',
+  'model' : '5672'
+}
+
+print 
+for k,v in net_device.items():
+  print k,v
+
+net_device['password'] = raw_input("Enter new password: ")
+net_device['secret'] = raw_input("Enter new secret: ")
+
+device_type = net_device.get('device_type', 'cisco_ios')
+print "\nDefault device type : {}\n".format(device_type)
